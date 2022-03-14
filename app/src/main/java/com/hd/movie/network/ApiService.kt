47b96a8bbb3d216d05d1041902ca32f6d820b362
lsl <lsl991126@163.com>
2022-03-14 11:15:19 +0800

    添加Retrofit

---
+++ app/src/main/java/com/hd/movie/network/ApiService.kt
@@ -0,0 +1,4 @@
+package com.hd.movie.network
+
+interface ApiService {
+}
---
+++ app/src/main/java/com/hd/movie/network/RetrofitUtils.kt
@@ -0,0 +1,30 @@
+package com.hd.movie.network
+
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitUtils {
+
+    fun getApiUrl(): ApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.pexels.com")
+            .client(okHttpClient())
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+
+    private fun okHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    }
+
+    private val loggingInterceptor: HttpLoggingInterceptor
+        get() {
+            val interceptor = HttpLoggingInterceptor { message -> Log.i("onResponse:", message) }
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            return interceptor
+        }
+}
